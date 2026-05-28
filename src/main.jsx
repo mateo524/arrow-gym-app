e4bc579df97264c8c954f9e098ce7d67909fea48
Mateo <sanchezmateo113@gmail.com>
2026-05-28 17:18:50 -0300

    Fix SW cache name v5 + force update on mobile

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from "./App.jsx";
 import "./styles.css";
 
 if ("serviceWorker" in navigator) {
-  window.addEventListener("load", () => navigator.serviceWorker.register("/sw.js").catch(() => {}));
+  window.addEventListener("load", () => navigator.serviceWorker.register("/sw.js?v=5").catch(() => {}));
 }
 
 createRoot(document.getElementById("root")).render(<App />);
