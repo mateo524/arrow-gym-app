ad5f236237057cc6d82296b52e5232cdf8f8f302
Mateo <sanchezmateo113@gmail.com>
2026-06-18 00:36:30 -0300

    Updates

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "arrow-gym-v10";
+const CACHE = "arrow-gym-v11";
 const BASE = "/arrow-gym-app";
 self.addEventListener("install", event => { event.waitUntil(caches.open(CACHE).then(cache => cache.addAll([BASE + "/", BASE + "/index.html"]))); self.skipWaiting(); });
 self.addEventListener("activate", event => { event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))); self.clients.claim(); });
