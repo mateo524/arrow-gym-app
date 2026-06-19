c9e9263906c344d3eee9f85c8a45cb72feebb2b3
Mateo <sanchezmateo113@gmail.com>
2026-06-19 11:07:56 -0300

    Updates

--- assets/vendor-icons-CGF_ee3w.js
+++ assets/vendor-icons-CGF_ee3w.js
@@ -1,4 +1,4 @@
-import{r as n}from"./vendor-state-BHkfXj5w.js";/**
+import{r as n}from"./vendor-state-LQcbr8d-.js";/**
  * @license lucide-react v1.17.0 - ISC
  *
  * This source code is licensed under the ISC license.
